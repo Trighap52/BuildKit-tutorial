9d769dd7e31f3e32fac02f44f29375d7defd1ce0
Trighap52 <Trighap52@gmail.com>
2024-10-06 15:55:01 +0200

    20th commit

--- scenarios/opa-tutorial/assets/verify1.sh
+++ scenarios/opa-tutorial/assets/verify1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Fetch all pod statuses in the gatekeeper-system namespace
-all_pods=$(kubectl get pods -n gatekeeper-system --no-headers)
+# Fetch the count of pods in the Running state in the gatekeeper-system namespace
+all_pods=$(kubectl get pods -n gatekeeper-system --no-headers | grep -c "Running")
 
 # Check if at least one pod is running
-if [[ -n "$all_pods" && $(echo "$all_pods" | grep -c "Running") -ge 1 ]]; then
+if [[ $all_pods -ne 0 ]]; then
   echo "OPA Gatekeeper is installed and at least one component is running successfully."
   exit 0
 else
